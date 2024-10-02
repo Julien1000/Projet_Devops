b95673a4e584aa6ae702945a2a3dbfb3343ae969
Lucas64000 <loustaletl@cy-tech.fr>
2024-10-02 16:19:39 +0200

    Requete suivant artiste

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -7,10 +7,22 @@ client = MongoClient('localhost', 27017)
 db = client.devOps_bdd
 collection = db.sons
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    sons = collection.find()
-    return render_template('index.html', sons=sons)
+    sons = []  
+    error = None  
+
+    if request.method == 'POST':
+        artist_name = request.form.get('artist_name')  
+        if artist_name:
+            songs = collection.find({'artistName': {'$regex': artist_name, '$options': 'i'}})  
+            sons = list(songs)
+            if not sons:
+                error = f"Aucun artiste trouvé avec le nom: '{artist_name}'"
+        else:
+            error = "Veuillez entrer un nom d'artiste."
+
+    return render_template('index.html', sons=sons, error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
