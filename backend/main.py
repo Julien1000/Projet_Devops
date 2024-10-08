16815c20671cfe7e5250f4a38595e2e897efd3aa
marcodamecourt <depontonda@cy-tech.fr>
2024-10-08 14:30:45 +0200

    mise en relation mongo

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,38 @@ from fastapi.templating import Jinja2Templates
 from typing import Optional
 import pandas as pd
 from data.generator import *
+import csv
+from pymongo import MongoClient
 
 app = FastAPI()
 router = APIRouter(prefix="/api/v1")
-# Charger et nettoyer les données Spotify
-df = pd.read_csv('./data/spotify_data.csv')
-df = clean_spotify_data_csv(df)
+
+username = 'Marco'  # Remplacez par votre nom d'utilisateur MongoDB
+password = 'jojopause123'     # Remplacez par votre mot de passe MongoDB
+host = 'localhost'                  # Remplacez par l'hôte de votre MongoDB, par ex. localhost ou IP distante
+port = '27017'                      # Remplacez par le port (habituellement 27017 par défaut)
+db_name = 'Database_spotify'           # Remplacez par le nom de votre base de données
+
+# URI de connexion avec authentification
+
+client = MongoClient(f'mongodb://admin:admin@mongodb:27017/')
+
+db = client['devOpsBDD']  # Remplacez par le nom de votre base de données
+collection = db['SpotifySongs']
+all_songs = list(collection.find())
+all_songs = clean_spotify_data_mongo(all_songs)
+
+
+# df0=pd.read_csv('./data/spotify_data.csv')
+# print(df0.iloc[14])
+
+# print('§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§')
+# print(all_songs.iloc[0])
+# print('§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§')
+
+
+
+
 
 # Configuration des templates
 templates = Jinja2Templates(directory="templates")
@@ -23,22 +49,25 @@ async def read_item():
 async def predict( query: Optional[str] = Form(None)):
     
     # Filtrer la chanson entrée par l'utilisateur dans le dataset
-    input_song = df[df['trackName'].str.contains(query, case=False)].iloc[0]
-    playlist = generate_playlist(df, input_song, 9)
-
-    print(playlist)
-    return {"message":query}
-    return {"message":input_song}
-    # Vérifier si la chanson existe dans les données
-    if input_song.empty:
-       
-        return templates.TemplateResponse("predict.html", {"request": request, "error": "Chanson non trouvée"})
-
-    # Générer une playlist basée sur la chanson d'entrée
-    playlist = generate_playlist(df, input_song, 9)
+    # Utilisation de la regex pour faire une recherche insensible à la casse dans MongoDB
+    # input_song = collection.find_one({"trackName": {"$regex": query, "$options": "i"}})
+    input_song = all_songs[all_songs['trackName'].str.contains(query, case=False)].iloc[0]
     
-    # Envoyer la playlist au front-end
-    return templates.TemplateResponse("predict.html", {"request": request, "playlist": playlist})
+# Affichage du premier résultat
+    if input_song:
+        print(input_song)
+    else:
+        print("Aucun résultat trouvé.")
+    
+    
+    print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
+    playlist = generate_playlist(all_songs, input_song, 9)
+    print(playlist)
+    print("////////////////////////////////////////////////////////////////////////////////////////////////")
+
+    return {"message"}
+    # Vérifier si la chanson existe dans les données
+   
 
 app.include_router(router)
 
